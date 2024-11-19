86c53617bb0f1a81a283169da79e215d06fcbd5f
theppasiva <theppasiva@gmail.com>
2024-11-19 23:09:05 +0530

    updated python version in bootstrap.sh

--- 04-databases/bootstrap.sh
+++ 04-databases/bootstrap.sh
@@ -1,6 +1,13 @@
+# #!/bin/bash
+# component=$1
+# environment=$2 #dont use env here, it is reserved in linux
+# yum install python3.11-devel python3.11-pip -y
+# pip3.11 install ansible botocore boto3
+# ansible-pull -U https://github.com/theppasiva/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment main-tf.yaml
+
 #!/bin/bash
 component=$1
 environment=$2 #dont use env here, it is reserved in linux
-yum install python3.11-devel python3.11-pip -y
-pip3.11 install ansible botocore boto3
-ansible-pull -U https://github.com/theppasiva/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment main-tf.yaml
+yum install python3.12-devel python3.12-pip -y
+pip3.12 install ansible ansible-core==2.16 botocore boto3
+ansible-pull -U https://github.com/theppasiva/roboshop-ansible-roles-tf-1.git  -e component=$component -e env=$environment main-tf.yaml
