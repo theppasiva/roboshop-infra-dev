b40f1adc71dcb79e5e05987ddba6ac5767cd055a
theppasiva <theppasiva@gmail.com>
2024-11-27 21:59:49 +0530

    updated provider.tf and variables.tf

--- 03-vpn/variables.tf
+++ 03-vpn/variables.tf
@@ -1,7 +1,7 @@
 variable "common_tags" {
     default = {
         Project = "roboshop"
-        Environment = "prod"
+        Environment = "dev"
         Terraform = "true"
     }
 }
@@ -11,5 +11,5 @@ variable "project_name" {
 }
 
 variable "environment" {
-  default = "prod"
+  default = "dev"
 }
